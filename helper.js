e9f408f16999b4d8f3ff7fc2133682614b2df5b9
lrb9986 <lrb9986@gmail.com>
2020-06-10 23:36:35 +0800

    Update helper.js

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ var date = Date.now();
 var data = {
   body: "{\"code\": 200,  \"msg\": \"请求成功\",  \"data\": {    \"free_search\": 1000000  },  \"timestamp\": "+date+"}",
   headers: {
-    "Content-Type": "multipart/byteranges"
+    "Content-Type": "application/json"
   }
 };
 $done({response: data});
