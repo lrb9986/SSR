45a4478db29818f2b107512f7e04799614edab21
lrb9986 <lrb9986@gmail.com>
2020-06-11 13:03:31 +0800

    Update helper.js

--- helper.js
+++ helper.js
@@ -6,9 +6,9 @@ var obj ={
     "search_num": 100000,
     "score": 100000,
     "invite_count": 100000,
-    "invite_level": "LV3",
-    "is_authentication": 2,
-    "is_invite": 2,
+    "invite_level": "LV1",
+    "is_authentication": -1,
+    "is_invite": -1,
     "buy_limit": 1,
     "discount": 100000
   },
