1e12f7c6f1b4c945a9078598dc5ef5e720d1c724
lrb9986 <lrb9986@gmail.com>
2020-06-11 09:31:17 +0800

    Update helper2.js

--- helper2.js
+++ helper2.js
@@ -3,10 +3,10 @@ var obj = JSON.parse($response.body);
 obj.data['free_search'] = 10000;
 obj.data['search_num'] = 10000;
 obj.data['invite_count'] = 100;
-obj.data['iinivite_level'] = "LV1";
-obj.data['is_authentication'] = 1;
-obj.data['is_invite'] = 1;
-obj.data['discount'] = 1;
+obj.data['invite_level'] = "LV1";
+obj.data['is_authentication'] = 2;
+obj.data['is_invite'] = 2;
+obj.data['discount'] = 2;
 obj.timestamp = Date.now();
 
 $done({body:JSON.stringify(obj)});
