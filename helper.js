8ebc0df8d43c10f4a32d081454f6a952f130e9fa
lrb9986 <lrb9986@gmail.com>
2020-06-11 09:54:57 +0800

    Update helper.js

--- helper.js
+++ helper.js
@@ -4,6 +4,7 @@ var obj ={
   "data": {
     "free_search": 100000,
     "search_num": 100000,
+    "score": 100000,
     "invite_count": 100000,
     "invite_level": "LV3",
     "is_authentication": -1,
